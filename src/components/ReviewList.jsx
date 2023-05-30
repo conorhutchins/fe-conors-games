a4e39f6f55b9f347291b3f80a8d35596efbe0157
conorhutchins <conorhutchins@btinternet.com>
2023-05-30 17:30:13 +0100

    adds css functionality to reviewList component

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { fetchReviews } from '../api';
-import '../styles/ReviewList.css';
+import '../styles/reviewList.css';
 
 const ReviewList = () => {
   const [reviews, setReviews] = useState([]);
@@ -19,24 +19,28 @@ const ReviewList = () => {
 
   return (
     <section className="review-list">
-      <h1>Here's all our reviews!</h1>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {reviews.map((review) => (
-            <div className="review-card" key={review.review_id}>
-              <h2>{review.title} as designed by {review.designer}</h2>
-              <h3>Current Owner: {review.owner}</h3>
-              <img src={review.review_img_url} alt={`An image of ${review.title}`} />
-              <h3>Category: {review.category}</h3>
-              <p>created at: {review.created_at}</p>
-              <p>votes: {review.votes}</p>
-              <p>comments: {review.comment_count}</p>
-            </div>
-          ))}
-        </>
-      )}
+      <div className="heading">
+        <h1>Here's all our reviews!</h1>
+      </div>
+      <div className="cards">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            {reviews.map((review) => (
+              <div className="review-card" key={review.review_id}>
+                <h2>{review.title} as designed by {review.designer}</h2>
+                <h3>Current Owner: {review.owner}</h3>
+                <img src={review.review_img_url} alt={`An image of ${review.title}`} />
+                <h3>Category: {review.category}</h3>
+                <p>created at: {review.created_at}</p>
+                <p>votes: {review.votes}</p>
+                <p>comments: {review.comment_count}</p>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 };
